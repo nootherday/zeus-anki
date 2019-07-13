72b890a45ec48cce0ddb471a49d9d7395e16b9e1
miles <miles@lendit.co.kr>
2019-07-13 14:52:49 +0900

    tts 라이브러리 추가

---
+++ tts.py
@@ -0,0 +1,54 @@
+import boto3
+from pydub import AudioSegment
+
+import sys
+import os
+
+class ZeusTTS():
+    def __init__(self, config):
+        session = boto3.Session(
+            aws_access_key_id = config['aws_access_key_id'],
+            aws_secret_access_key = config['aws_secret_access_key'])
+        self._voice_ids = config["voice_ids"]
+        self._interval_in_seconds = config["interval_in_seconds"]
+        self._path = os.path.abspath(config["path"])
+        self._polly = session.client('polly', region_name=config["aws_region"])
+
+    def generate(self, text):
+        filename = os.path.join(self._path, text.lower().replace(" ", "_") + ".mp3")
+
+        if os.path.exists(filename):
+            return
+
+        print (f"generate {text}")
+
+        temp_filename = os.path.join(self._path, "_temp.mp3")
+        audio = None
+        for voice_id in self._voice_ids:
+            response = self._polly.synthesize_speech(
+                VoiceId = voice_id,
+                TextType = 'ssml',
+                OutputFormat = 'mp3', 
+                Text = f'<speak><prosody rate="slow">{text}</prosody></speak>')
+
+            file = open(temp_filename, 'wb')
+            file.write(response['AudioStream'].read())
+            file.close()
+
+            cur = AudioSegment.from_mp3(temp_filename)
+            if audio == None:
+                audio = cur
+            else:
+                audio = audio + AudioSegment.silent(duration=self._interval_in_seconds * 1000) + cur
+
+        audio.export(filename, format="mp3")
+        os.remove(temp_filename)
+
+if __name__ == "__main__":
+    import json
+
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+        tts = ZeusTTS(config["tts"])
+        tts.generate("my test")
